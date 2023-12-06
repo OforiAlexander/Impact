b5b804c7d8f9b07395b290a335ebe378756fb4dc
OforiAlexander <alexanderofori07@gmail.com>
2023-12-06 15:56:33 +0000

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,23 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        'raleway': ['Raleway', 'sans-serif']
+        'raleway': ['Raleway', 'sans-serif'],
+        'poppins': ['Poppins', "sans-serif"]
       },
       backgroundColor: {
         'light_gray': '#f6f7fd',
-        'btn_blue': '#0090ff'
+        'btn_blue': '#0090ff',
+        'txt_color': '#0e1b4d'
       },
       textColor: {
         'logo_ace': '#0090ff',
         'nav_link': '#20252a',
-        'txt_color': '#0e1b4d'
+        'txt_color': '#0e1b4d',
+        'txt_pink': '#df42b1'
             },
         borderColor: {
-          'bdr_blue': '#0090ff'
+          'bdr_blue': '#0090ff',
+          'txt_pink': '#df42b1'
         },
         borderRadius: {
           'xxl': '3.35rem'
